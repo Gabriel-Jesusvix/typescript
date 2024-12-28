8a6edf98a84739de64e66815cd067251cd227f3c
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2024-12-27 23:12:21 -0300

    feat: understand type null and undefined

---
+++ src/type_null.ts
@@ -0,0 +1,19 @@
+
+/* 
+  type null in the typescript is used to represent the null value.
+*/
+
+let name: string | null;
+
+name = "John";
+
+name = null;
+
+let undefinedValue;
+
+console.log(undefinedValue); // undefined
+
+/*
+  undefined is a type in typescript that represents the undefined value.
+  don't defined value to a variable, it will be undefined.
+*/
