54fb371e8df92528a21a8ff1a188a9cd4940b927
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2024-12-27 23:16:55 -0300

    feat: understand type unknow

---
+++ src/type_unknow.ts
@@ -0,0 +1,16 @@
+
+/*
+  type unknow in the typescript is used to represent the unknown value.
+  when we don't know the type of the value, we can use the unknown type.
+*/
+
+
+let total: unknown;
+
+total = 100;
+
+total = "250";
+
+total = {
+  amount: 350
+}
