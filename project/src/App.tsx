c8fc340e6b76832eeb9121e412714adf4dc9cb31
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2025-02-06 16:00:26 -0300

    feat: implement form with hooks

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,19 +1,53 @@
+import { useRef, useState } from "react"
 import { Header } from "./components/Header"
-import { Name } from "./components/Name"
 
 
 
 function App() {
+  const nameRef = useRef<HTMLInputElement>(null)
+  const [users, setUsers] = useState<string[]>([''])
+
+
+  function handleRegister(e: React.FormEvent) {
+    e.preventDefault();
+    if(!nameRef.current?.value) {
+      alert('Digite seu nome')
+      return;
+    }
+
+    const name = nameRef.current?.value
+
+    setUsers(values => [...values, name])
+  }
+
+
   return (
     <div>
       <Header
         label="TS + React"
         description="Aplicando TypeScript no React"
       />
-      <Name
-        name="G.J"
-        age={26}
-      />
+
+      <form action="" onSubmit={handleRegister}>
+        <label>Nome:</label>
+        <input
+          type="text"
+          placeholder="Digite seu nome..."
+          ref={nameRef}
+        />
+        <button type="submit">Cadastrar</button>
+      </form>
+
+      {
+        users.map((user, index) => (
+          <div>
+            <span key={index}>{user}</span>
+            <br />
+          </div>
+        ))
+      }
+
+
     </div>
   )
 }
